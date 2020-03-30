82942521cefb8ffeb923d8b75e0da12c59301be4
Sean Dever <seand0611@gmail.com>
2020-03-30 20:05:53 +0000

    Automatically backed up by Learn

---
+++ lib/simple_loops.rb
@@ -0,0 +1,14 @@
+def loop_message_five_times(string)
+  count = 0
+  while count < 5 do
+    puts string
+    count += 1
+end
+
+def loop_message_n_times(string, interger)
+  count = 0 
+  while array[count] do
+    puts array[count]
+    count += 1 
+end
+
