c906580bcd6e92e58aba78a2e50eb14ab7593340
Sean Dever <seand0611@gmail.com>
2020-03-30 20:31:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,7 +26,7 @@ end
 def return_string_array(array)
   count = 0
   new_array = []
-  while array[count] < array.length do
+  while count < array.length do
     new_array.push(array[count].to_s)
     count += 1
   end 
